b50ee1be0e9dd09665b5e8842f2e4871a3b0744c
Alen Siljak <alen.siljak@yahoo.com>
2017-09-24 22:43:46 +0200

    messages during deployment

--- script/deploy.cmd
+++ script/deploy.cmd
@@ -1,8 +1,17 @@
 :: after the build, move the files to /docs folder and commit. They will be published to GitHub Pages automatically.
 
+@echo off
+
+echo cleaning up.
 :: clean up first
 del /q destination\*
-for /d %x in (destination\*) do @rd /s /q "%x"
+for /d %%x in (destination\*) do @rd /s /q "%%x"
+
+echo press any key to deploy
+pause
 
 :: copy newly generated content
 xcopy ..\build\* ..\docs\ /e
+
+:: manual adjustment
+del %destination%\favicon.ico.bak
