83b1f407b85c7a3ad8a7daab548be8c5ed3ab387
Alen Siljak <alen.siljak@yahoo.com>
2017-09-24 22:44:53 +0200

    using a variable for destination directory

--- script/deploy.cmd
+++ script/deploy.cmd
@@ -2,16 +2,18 @@
 
 @echo off
 
+set destination=..\docs
+
 echo cleaning up.
 :: clean up first
-del /q destination\*
-for /d %%x in (destination\*) do @rd /s /q "%%x"
+del /q %destination%\*
+for /d %%x in (%destination%\*) do @rd /s /q "%%x"
 
 echo press any key to deploy
 pause
 
 :: copy newly generated content
-xcopy ..\build\* ..\docs\ /e
+xcopy ..\build\* %destination% /e
 
 :: manual adjustment
 del %destination%\favicon.ico.bak
