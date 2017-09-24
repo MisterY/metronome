5fbfd25ec951ed089bd720d8f1883e4c8220a043
Alen Siljak <alen.siljak@yahoo.com>
2017-09-24 22:32:38 +0200

    cleaning up the destination first

--- script/deploy.cmd
+++ script/deploy.cmd
@@ -1,5 +1,8 @@
 :: after the build, move the files to /docs folder and commit. They will be published to GitHub Pages automatically.
 
+:: clean up first
+del /q destination\*
+for /d %x in (destination\*) do @rd /s /q "%x"
 
-
+:: copy newly generated content
 xcopy ..\build\* ..\docs\ /e
