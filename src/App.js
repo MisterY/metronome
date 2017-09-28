6ead14751e5bad1762c64babff08e5637bfba96d
Alen Siljak <alen.siljak@yahoo.com>
2017-09-28 23:05:44 +0200

    adding github ribbon

--- src/App.js
+++ src/App.js
@@ -32,6 +32,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <a href="https://github.com/MisterY/metronome">
+            <img style={{position: "absolute", top: 0, right: 0, border: 0}} src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" />
+          </a>
+
           <img src={logo} className="App-logo" alt="logo" />
           <div style={{ marginLeft: 2 + "em" }}>
             <h1>Alen's Metronome</h1>
@@ -64,7 +68,7 @@ class App extends Component {
           orientation="vertical"
           //onChangeStart={this.handleChangeStart}
           onChange={this.changeVolume}
-        //onChangeComplete={this.handleChangeComplete} 
+          //onChangeComplete={this.handleChangeComplete} 
           ref={(x) => { this._volumeControl = x; }}
         />
         {/* </div>
